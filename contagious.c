d7194b23971090ae61bf681f0c00b47adad70d10
Maheshwar Anup <137691824+djmahe4@users.noreply.github.com>
2025-03-19 06:18:55 +0530

    Create contagious.c

---
+++ contagious.c
@@ -0,0 +1,32 @@
+#include<stdio.h> 
+
+void main(){ 
+
+int i,j,n,block[20],start; 
+
+printf("Enter the no. of file:\n"); 
+
+scanf("%d",&n); 
+
+printf("Enter the number of blocks needed for each file: \n"); 
+
+for(i-0;i<n;it+) 
+
+scanf("%d",&block[i)); 
+
+start=0; 
+
+printf("t\tFile name\tStart\tSize of file\t\t\n"); 
+
+printf("\nlt\tFile 1\t\t%dt\t%dn",start,block 
+
+[0); 
+
+for(i=2;i<=n;it+){ 
+
+start=start+block[i-2]; 
+
+printf("\t\tFile%d\t\t%d\t\t%dn",i,start,blo 
+
+ck[i-1]);
+}}
