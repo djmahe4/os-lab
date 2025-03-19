bb87f8fce267092ee4f27b6889505c69c922df5f
Maheshwar Anup <137691824+djmahe4@users.noreply.github.com>
2025-03-19 21:01:43 +0530

    Update lru.c

--- lru.c
+++ lru.c
@@ -9,12 +9,16 @@ count =0;
 printf("Enter the number of page frames: "); 
 scanf("%d", &n); 
 if (n < 1|| n> MIN) { 
-printf("\ninvalid number of frames. be between 1 and %d.\n", MIN); return; 
+printf("\ninvalid number of frames. be between 1 and %d.\n", MIN); 
+return;
+}
 printf("nEnter the reference string (-1 to end): "); 
 scanf("%d", &temp); 
-while (temp != -1){ ref[countt+] = temp; if (count >= MAX) { 
-printf("\nMaximum reference string size (%d) reached.\n", MAX); 
-break; 
+while (temp != -1){ 
+  ref[countt+] = temp; 
+  if (count >= MAX) { 
+    printf("\nMaximum reference string size (%d) reached.\n", MAX); 
+    break; 
 } 
 scanf("%d", &temp);
 }
@@ -33,23 +37,23 @@ if (n <1 || count < 1) {
 printf("Please input the reference string and frames first. \n"); 
 
 return; 
-
-for (i = 0; i < n; it+) { 
+}
+for (i = 0; i < n; i++) { 
 
 stack[i] = -1; 
-
+}
 for (i = 0; i < count; i++) { 
 
 int found = 0; 
 
-for (j = 0; j < top; jt+) { 
+for (j = 0; j < top; j++) { 
 
 if (stack[j] == ref[i) { 
 
 found = 1; 
 
 break; 
-
+}}
 if (found) { 
 
 int temp = stack[j]; 
@@ -58,21 +62,22 @@ for (k = j; k< top- 1; k++) {
 
 stack[k] = stack[k+ 1];
 }
-stack[top - 1] = temp; }else {
+stack[top - 1] = temp; }
+else {
 fault++; 
 if (top < n) { 
 stack[top++] = ref[il; 
 } else { 
 for (k = 0; k<n -1; k++) { 
 stack[k]= stack[k+ 1]; 
+}
 stack[n - 1] = ref[i]; 
 }}
-printf("\nAfter inserting %d, the stack status is:", ref[il); 
+printf("\nAfter inserting %d, the stack status is:", ref[i]); 
 for (j = 0; j < top; j++) { 
 printf("%d", stack[j); 
 }}
-printf("\nEnd of reference string 
-insertion."); 
+printf("\nEnd of reference string insertion."); 
 printf("nTotal page faults: %d.n", fault); 
 }
 int main(){ 
@@ -92,13 +97,13 @@ switch (choice) {
 
 Case 1: 
 
-input); 
+input(); 
 
 break; 
 
 Case 2: 
 
-LRU); 
+LRU(); 
 
 break; 
 
